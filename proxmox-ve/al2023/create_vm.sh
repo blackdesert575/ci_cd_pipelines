e4d4f6f4ca4ffa347d3528b7346a6cd6f31461a6
Hong <hong@ubuntu-lab-01>
2024-02-20 23:44:16 +0800

    Fix al2023

--- proxmox-ve/al2023/create_vm.sh
+++ proxmox-ve/al2023/create_vm.sh
@@ -7,5 +7,5 @@
 #  <vmid>: <integer> (100 - 999999999) 
 vmid=105
 
-qm create $vmid --name "amzn2" --memory 4096 --cores 2 --net0 virtio,bridge=vmbr0 -cdrom local:iso/seed.iso
+qm create $vmid --name "al2023" --memory 4096 --cores 2 --net0 virtio,bridge=vmbr0 -cdrom local:iso/seed.iso
 qm importdisk $vmid /var/lib/vz/template/iso/al2023-kvm-2023.3.20240205.2-kernel-6.1-x86_64.xfs.gpt.qcow2 local-lvm --format qcow2
