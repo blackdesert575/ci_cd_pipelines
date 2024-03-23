4936d837391fc87aa452f1a383b6e8198d7c8bc8
Hong <hong@ubuntu-lab-01>
2024-03-24 02:07:47 +0800

    update proxmox al2023 seedconfig

--- proxmox-ve/al2023/create_vm.sh
+++ proxmox-ve/al2023/create_vm.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# refs
-# https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/amazon-linux-2-virtual-machine.html#amazon-linux-2-virtual-machine-boot
+# refs:
+# https://docs.aws.amazon.com/linux/al2023/ug/kvm-supported-configurations.html#kvm-host-requirements
 # https://forum.proxmox.com/threads/how-to-run-qcow2-file-in-proxmox.70371/
+# kvm qcow2
+# https://cdn.amazonlinux.com/al2023/os-images/2023.4.20240319.1/kvm/
 
 #  <vmid>: <integer> (100 - 999999999) 
 vmid=105
+al2023_qcow2=al2023-kvm-2023.3.20240205.2-kernel-6.1-x86_64.xfs.gpt.qcow2
 
-qm create $vmid --name "al2023" --memory 4096 --cores 2 --net0 virtio,bridge=vmbr0 -cdrom local:iso/seed.iso
-qm importdisk $vmid /var/lib/vz/template/iso/al2023-kvm-2023.3.20240205.2-kernel-6.1-x86_64.xfs.gpt.qcow2 local-lvm --format qcow2
+qm create $vmid --name "al2023" --memory 4096 --cores 2 --cpu cputype=host --net0 virtio,bridge=vmbr0 -cdrom local:iso/seed.iso
+qm importdisk $vmid /var/lib/vz/template/iso/$al2023_qcow2 local-lvm --format qcow2
--- proxmox-ve/al2023/seedconfig/ceate_seed_iso.sh
+++ proxmox-ve/al2023/seedconfig/ceate_seed_iso.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# refs
+# https://cloudinit.readthedocs.io/en/latest/reference/examples.html
+# https://docs.aws.amazon.com/linux/al2023/ug/seed-iso.html
+
+# mkisofs -output seed.iso -volid cidata -joliet -rock user-data meta-data
+# mkisofs -output seed.iso -volid cidata -joliet -rock user-data meta-data network-config
 genisoimage -output seed.iso -volid cidata -joliet -rock user-data meta-data
+# genisoimage -output seed.iso -volid cidata -joliet -rock user-data meta-data network-config
 mv seed.iso /var/lib/vz/template/iso/
