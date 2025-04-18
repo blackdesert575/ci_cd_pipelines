154ad02a97ad737f001d2330d4b0602256c5c970
Hong <hong@ubuntu-lab-01>
2025-04-19 00:18:48 +0800

    Add tini for valid `init` for containers

--- docker/provisioning/devops/build.sh
+++ docker/provisioning/devops/build.sh
@@ -3,3 +3,10 @@ set -euxo pipefail
 
 docker build -f Dockerfile.python_test -t devops:test .
 # docker run -it --name devops devops:test
+
+# docker compose build --no-cache
+
+# docker compose up -d
+
+# check process
+ps -p 1 -o cmd=
