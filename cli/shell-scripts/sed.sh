a53be1728d90ef622f42d3b733f39474c5650f3a
hong539 <yunghunghuang984@gmail.com>
2025-10-05 10:33:45 +0800

    Update .git/config, scripts/**

--- cli/shell-scripts/sed.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sed -i 's/old-string/new-string/g' *.yaml
---
+++ scripts/bash/sed.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sed -i 's/old-string/new-string/g' *.yaml
+
+sed -i 's/git@github.com:hong539/git@github.com:blackdesert575/g' ~/repos/**/.git/config
