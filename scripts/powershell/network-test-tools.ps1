56d0ef7762f97c238f581f2afa20da18a85e54d2
hong539 <yunghunghuang984@gmail.com>
2026-05-09 11:58:08 +0800

    Add scripts/powershell/network-test-tools.ps1

---
+++ scripts/powershell/network-test-tools.ps1
@@ -0,0 +1,6 @@
+
+Test-NetConnection -ComputerName example.com -Port 443
+
+Test-NetConnection -ComputerName example.com -Port 80
+
+Test-NetConnection -ComputerName example.com -Port 80 -DiagnoseRouting -InformationLevel Detailed
